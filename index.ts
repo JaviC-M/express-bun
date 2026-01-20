aa8248bbd79f370c09bd98c33dc045a909f80f45
JaviC-M <pikajavi11@gmail.com>
2026-01-20 11:16:58 +0100

    creamos getmoviesbyid y getmoviesbytitle

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import { initDB } from "./db/init";
-import { getAllMovies } from "./models/movies";
+import { getAllMovies, getAllTitulos, getMovieById } from "./models/movies";
 
 const db = await initDB()
+
 //const peliculass = getAllMovies(db)
 //console.log(peliculass)
+
+//const peliculaId = getMovieById(db, 1)
+//console.log(peliculaId)
+
+const peliculaTitulo = getAllTitulos(db, "Toy")
+console.log(peliculaTitulo)
--- models/movies.ts
+++ models/movies.ts
@@ -4,3 +4,13 @@ export function getAllMovies(db: Database){
     const query = db.query("SELECT * FROM peliculas")
     return query.all()
 }
+
+export function  getMovieById(db: Database, id: number){
+    const query = db.query("SELECT * FROM peliculas WHERE id = ?")
+    return query.get(id)
+}
+
+export function getAllTitulos(db: Database, nombre: string){
+    const query = db.query("SELECT * FROM peliculas WHERE nombre LIKE ?")
+    return query.all(`%${nombre}%`)
+}
